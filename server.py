a9c853134f3d9b865edf3f26a90f855b1b377d00
platon.yasev <yasevplaton@gmail.com>
2018-09-20 21:19:37 +0300

    some refactoring

--- server.py
+++ server.py
@@ -9,7 +9,6 @@ The script for the distribution of data from the table of flows of goods through
 # import necessary modules
 import pandas as pd
 import geopandas as gpd
-import networkx as nx
 import os.path
 from tools import tools
 
@@ -30,97 +29,23 @@ roads["src"] = 0
 roads["dest"] = 0
 roads["length"] = roads.length
 
-# bind points and roads
-i = 0
-while i < len(roads):
-    var = points.geometry.intersects(roads.loc[i, 'geometry'])
-    nodes = points.OBJECTID[var.values]
-    roads.loc[i, "src"] = nodes.iloc[0]
-    roads.loc[i, "dest"] = nodes.iloc[1]
-    i += 1
+# bind points to roads
+roads = tools.bind_points_to_lines(points, roads)
 
 # collect types of goods
-goods = []
-for good in matrix_array:
-    goods.append(good['type'])
+goods = tools.collect_goods_types(matrix_array)
 
 # create oriented multigraph and fill it with edges from roads
-MG = nx.MultiGraph()
-
-for g, ord in zip(goods, range(len(goods))):
-    G = nx.from_pandas_edgelist(roads, source="src", target="dest", edge_attr=["ID_line", "length"])
-    nx.set_edge_attributes(G, 0, g)
-    nx.set_edge_attributes(G, ord, 'ORD')
-    MG.add_edges_from(G.edges.data())
-    G.clear()
-
-net = MG.to_directed()
+net = tools.create_graph(goods, roads)
 
 # determine the affiliation of a node to a city
-values = {}
-for n in range(len(points)):
-    values[points.OBJECTID[n]] = points.NAME[n]
-
-nx.set_node_attributes(net, values, 'city')
+net = tools.add_city_affiliation_attr(points, net)
 
 # distribute values of flows of goods on graph's edges
-for s, ds in net.nodes.items():
-    # if source and destination nodes are not junctions
-    if ds['city'] != 'junction':
-        for t, dt in net.nodes.items():
-            if dt['city'] != 'junction':
-                # create the shortest path between source and destination
-                route = nx.shortest_path(net, source=s, target=t, weight="length")
-                # create path graph from found nodes
-                H = nx.path_graph(route)
-                for e in H.edges:
-                    j = 0
-                    for g, m in zip(goods, matrix_array):
-                        # if source node is not destination node
-                        if s != t:
-                            # increase value of specific type of good by value from matrix of this type of good
-                            net.edges[e[0], e[1], j][g] = net.edges[e[0], e[1], j][g] + m['data'].loc[s, t]
-                            j += 1
-                H.clear()
+net = tools.distribute_values_on_graph(net, goods, matrix_array)
 
 # create dataframe from multigraph
-sources = []
-destination = []
-id_line = []
-types = []
-values = []
-order = []
-
-for n1, n2, dattr in net.edges.data():
-    sources.append(n1)
-    destination.append(n2)
-    id_line.append(dattr['ID_line'])
-    order.append(dattr['ORD'])
-    for k, v in dattr.items():
-        if k in goods:
-            types.append(k)
-            values.append(v)
-
-datastore = {'src': sources,
-             'dest': destination,
-             'ID_line': id_line,
-             'type': types,
-             'value': values,
-             'order': order}
-
-edges_df = pd.DataFrame(datastore, columns=['src', 'dest', 'ID_line', 'type', 'value', 'order'])
-edges_df = edges_df.sort_values(by=['ID_line', 'src', 'dest']).copy()
-edges_df.index = range(len(edges_df))
-edges_df['dir'] = 0
-edges_df = edges_df[['src', 'dest', 'ID_line', 'type', 'value', 'dir', 'order']].copy()
-
-# fill direction column with ids of direction
-for s in edges_df.index:
-    var = edges_df.src[s] - edges_df.dest[s]
-    if var < 0:
-        edges_df.loc[s, 'dir'] = 1
-    else:
-        edges_df.loc[s, 'dir'] = -1
+edges_df = tools.create_dataframe_from_graph(net, goods)
 
 # create geodataframe by merging dataframe and roads geometry
 road_geometry = roads[['ID_line', 'geometry']]
@@ -128,16 +53,7 @@ edges_df = edges_df.merge(road_geometry, on='ID_line')
 geo_edges = gpd.GeoDataFrame(edges_df, crs=roads.crs, geometry='geometry')
 
 # reverse order of nodes in line if a first node in line is not a source node
-for l in range(len(geo_edges)):
-    src_id = geo_edges.src[l]
-    src = points[(points.OBJECTID == src_id)]
-    src_geom = src.geometry.iloc[0]
-    x_src = src_geom.x
-    y_src = src_geom.y
-    line_geom = geo_edges.geometry.iloc[l]
-    first_node = line_geom.coords[0]
-    if first_node[0] != x_src or first_node[1] != y_src:
-        geo_edges.loc[l, 'geometry'] = tools.reverse_geometry_line(line_geom)
+geo_edges = tools.reverse_nodes_order(geo_edges, points)
 
 # reproject edges to 4326
 geo_edges = geo_edges.to_crs({'init': 'epsg:4326'})
--- tools/tools.py
+++ tools/tools.py
@@ -2,6 +2,7 @@
 
 import pandas as pd
 from shapely.geometry import LineString
+import networkx as nx
 
 '''
 
@@ -78,6 +79,125 @@ Other functions
 '''
 
 
+# function to bind points to lines
+def bind_points_to_lines(points, lines):
+    binded_lines = lines
+    i = 0
+    while i < len(binded_lines):
+        var = points.geometry.intersects(binded_lines.loc[i, 'geometry'])
+        nodes = points.OBJECTID[var.values]
+        binded_lines.loc[i, "src"] = nodes.iloc[0]
+        binded_lines.loc[i, "dest"] = nodes.iloc[1]
+        i += 1
+
+    return binded_lines
+
+
+# function to collect types of goods
+def collect_goods_types(matrix_array):
+    goods_array = []
+    for good in matrix_array:
+        goods_array.append(good['type'])
+
+    return goods_array
+
+
+# function to create oriented multigraph
+def create_graph(goods_array, lines):
+    MG = nx.MultiGraph()
+
+    for g, ord in zip(goods_array, range(len(goods_array))):
+        temp_graph = nx.from_pandas_edgelist(lines, source="src", target="dest", edge_attr=["ID_line", "length"])
+        nx.set_edge_attributes(temp_graph, 0, g)
+        nx.set_edge_attributes(temp_graph, ord, 'ORD')
+        MG.add_edges_from(temp_graph.edges.data())
+        temp_graph.clear()
+
+    net = MG.to_directed()
+
+    return net
+
+
+# function to add attribute of affiliation of a node to a city
+def add_city_affiliation_attr(points, graph):
+    values = {}
+    for n in range(len(points)):
+        values[points.OBJECTID[n]] = points.NAME[n]
+
+    nx.set_node_attributes(graph, values, 'city')
+
+    return graph
+
+
+# function to distribute values of flows on graph
+def distribute_values_on_graph(graph, goods_array, matrix_array):
+    for s, ds in graph.nodes.items():
+        # if source and destination nodes are not junctions
+        if ds['city'] != 'junction':
+            for t, dt in graph.nodes.items():
+                if dt['city'] != 'junction':
+                    # create the shortest path between source and destination
+                    route = nx.shortest_path(graph, source=s, target=t, weight="length")
+                    # create path graph from found nodes
+                    path_graph = nx.path_graph(route)
+                    for e in path_graph.edges:
+                        # set counter for edge
+                        j = 0
+                        for g, m in zip(goods_array, matrix_array):
+                            # if source node is not destination node
+                            if s != t:
+                                # increase value of specific type of good by value from matrix of this type of good
+                                graph.edges[e[0], e[1], j][g] = graph.edges[e[0], e[1], j][g] + m['data'].loc[s, t]
+                                # increment counter
+                                j += 1
+                    path_graph.clear()
+
+    return graph
+
+
+# function to create frame from filled graph
+def create_dataframe_from_graph(graph, goods_array):
+    sources = []
+    destination = []
+    id_line = []
+    types = []
+    values = []
+    order = []
+
+    for n1, n2, dattr in graph.edges.data():
+        sources.append(n1)
+        destination.append(n2)
+        id_line.append(dattr['ID_line'])
+        order.append(dattr['ORD'])
+        for k, v in dattr.items():
+            if k in goods_array:
+                types.append(k)
+                values.append(v)
+
+    datastore = {'src': sources,
+                 'dest': destination,
+                 'ID_line': id_line,
+                 'type': types,
+                 'value': values,
+                 'order': order}
+
+    dataframe = pd.DataFrame(datastore, columns=['src', 'dest', 'ID_line', 'type', 'value', 'order'])
+    dataframe = dataframe.sort_values(by=['ID_line', 'src', 'dest']).copy()
+    dataframe.index = range(len(dataframe))
+    dataframe['dir'] = 0
+    dataframe = dataframe[['src', 'dest', 'ID_line', 'type', 'value', 'dir', 'order']].copy()
+
+    # fill direction column with ids of direction
+    for s in dataframe.index:
+        var = dataframe.src[s] - dataframe.dest[s]
+        if var < 0:
+            dataframe.loc[s, 'dir'] = 1
+        else:
+            dataframe.loc[s, 'dir'] = -1
+
+    return dataframe
+
+
 # function to reverse line geometry
 def reverse_geometry_line(line):
     coord_arr = []
@@ -88,3 +208,19 @@ def reverse_geometry_line(line):
         rev_arr.append(i)
     rev_line = LineString(rev_arr)
     return(rev_line)
+
+
+# function to reverse order of nodes in line
+def reverse_nodes_order(geodataframe, points):
+    for edge in range(len(geodataframe)):
+        src_id = geodataframe.src[edge]
+        src = points[(points.OBJECTID == src_id)]
+        src_geom = src.geometry.iloc[0]
+        x_src = src_geom.x
+        y_src = src_geom.y
+        line_geom = geodataframe.geometry.iloc[edge]
+        first_node = line_geom.coords[0]
+        if first_node[0] != x_src or first_node[1] != y_src:
+            geodataframe.loc[edge, 'geometry'] = reverse_geometry_line(line_geom)
+
+    return geodataframe
